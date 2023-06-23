88b78e7ae421f475efa6dfb52b1a60c89cb5d7d0
Amir <amir.dikhanbayev@gmail.com>
2023-06-23 15:12:42 +0600

    first commit

---
+++ src/main/java/com/example/demo/Controller/TaskController.java
@@ -0,0 +1,62 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Repository.TaskRepository;
+import com.example.demo.model.Task;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+    @PostMapping("/tasks")
+    public Task create(@RequestBody Task task){
+        return taskRepository.save(task);
+    }
+    @GetMapping("/tasks")
+    public List<Task> getAll(){
+        return taskRepository.findAll();
+    }
+    @GetMapping("/tasks/{id}")
+    public Task takeId(@PathVariable Long id){
+        return taskRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());};
+    @PutMapping("/tasks")
+    public Task change(@RequestBody Task task) throws EntityNotFoundException{
+        Task old = taskRepository.findById(task.getId()).orElse(new Task());
+        old.setId(task.getId());
+        Optional.ofNullable(task.getDate()).ifPresent(old::setDate);
+        Optional.ofNullable(task.getDescription()).ifPresent(old::setDescription);
+        Optional.ofNullable(task.isDone()).ifPresent(old::setDone);
+        return taskRepository.save(old);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        taskRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/tasks/{id}:mark-as-done")
+    public void patchMethod(@PathVariable Long id){
+        taskRepository.MakeAsDone(id);
+    }
+
+    @PatchMapping("/task/{id}")
+    public void patchMethod(@PathVariable Long id, @RequestBody Task task){
+        if(task.isDone()){
+            taskRepository.MakeAsDone(id);
+        }
+    }
+
+    @PatchMapping("/tasks/{id}:mark-as-false")
+    public void patchMethod2(@PathVariable Long id){
+        taskRepository.MakeAsFalse(id);
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/Repository/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.Repository;
+
+import com.example.demo.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Modifying
+    @Query("UPDATE Task t SET t.done = TRUE WHERE t.id = :id")
+    public void MakeAsDone(@Param("id")Long id);
+
+    @Modifying
+    @Query("UPDATE Task t set t.done = FALSE WHERE t.id = :id")
+    public void MakeAsFalse(@Param("id")Long id);
+
+}
